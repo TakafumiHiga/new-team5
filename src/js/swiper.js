1c5593f7e10e570bcef6c69bf18cbc4086a39b5f
yumauda <firststep.yumauda@gmail.com>
2022-04-10 21:20:19 +0900

    2022.04.10 21:00 swiper 設定追加

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,30 +1,18 @@
+'use strict';
 
-
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-
-  var slider1 = new Swiper ('.slider1', {
+  var slider1 = new Swiper ('.js-mv-swiper', {
     loop: true,
     effect: 'fade',
+    speed: 2000,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+});
+
+  var slider2 = new Swiper ('.js-works-swiper', {
+    loop: true,
+    effect: 'horizontal',
     speed: 2000,
     autoplay: {
       delay: 4000,
